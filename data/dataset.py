b5b81bcc9f4e59aee3090d32e581c16c0f32a383
Chaitanya Tatipigari <ctatipigari3@gatech.edu>
2025-04-09 18:50:49 -0700

    organized dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -6,57 +6,59 @@ from torch.utils.data import Dataset
 from random import randrange
 from torchvision.io import decode_image
 
-
-# Dataset Class
 class StyleTransferDataset(Dataset):
+    """
+    Dataset class for style transfer training.
+    """
+
     def __init__(
         self,
-        style_path,
-        content_path,
+        style_dir,
+        content_dir,
         num_styles_per_image=100
     ):
+        """
+        Init dataset with style/content paths and sampling setup.
+        """
         super().__init__()
         self.num_styles_per_image = num_styles_per_image
         self.style_paths = {}
 
-        #   If need to extract information from file name
-        #   import re
-        #   pattern = re.compile(r'^(.*?)_(.*?)_(\d+)\.jpg$')  
-        #   e.g. match = pattern.match(filename); artist_name = match.group(3)
-
-        ##   STYLE FILEPATHS FOR RANDOM SAMPLING   ##
-        # Use glob to get all .jpg files in the folder
-        self.style_paths = {}   # storing style image paths
-        style_paths = glob.glob(os.path.join(style_path, '*.jpg'))
+        # Load style image paths
+        style_paths = glob.glob(os.path.join(style_dir, '*.jpg'))
         self.total_style_images = len(style_paths)
         for i, filepath in enumerate(style_paths):
             self.style_paths[i] = filepath
-            i += 1
 
-        ##   CONTENT FILEPATHS  ##
-        # Use glob to get all .jpg files in the folder
-        self.content_paths = {}   # storing style image paths
-        content_paths = glob.glob(os.path.join(content_path, '*.jpg'))
+        # Load content image paths
+        self.content_paths = {}
+        content_paths = glob.glob(os.path.join(content_dir, '*.jpg'))
         self.total_content_images = len(content_paths)
         for i, filepath in enumerate(content_paths):
             self.content_paths[i] = filepath
-        
-        # Construct indices for content images (repeating self.num_styles_per_image times)
+
+        # Prepare indices for sampling
         self.all_inds = self._build_indices()
-    
+
     def _build_indices(self):
-        # Construct indices for the dataset class
+        """
+        Build repeated and shuffled content indices.
+        """
         base_inds = np.arange(self.total_content_images)
         all_inds = np.repeat(base_inds, self.num_styles_per_image)
-        np.random.shuffle(all_inds)   # shuffles in-place
+        np.random.shuffle(all_inds)
         return all_inds
 
     def __len__(self):
-        # Return the total number of samples
+        """
+        Return dataset length.
+        """
         return len(self.all_inds)
 
     def __getitem__(self, idx):
-        # Retrieve a sample by index
+        """
+        Return a sample with style and random content image.
+        """
         style = decode_image(self.style_paths[self.all_inds[idx]])
         content = decode_image(self.content_paths[randrange(self.total_content_images)])
         sample = {
