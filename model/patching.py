c785117ba423a303b9492bec231c981d3a8694c2
Chaitanya Tatipigari <ctatipigari3@gatech.edu>
2025-04-19 21:58:49 -0700

    Update patching.py

--- model/patching.py
+++ model/patching.py
@@ -6,23 +6,24 @@ class ContentPatching(nn.Module):
     Content Patching class for patching content image into patches (i,j)
     Content images will be fed in as batch x 3 x 150 x 150
     '''
-    def __init__(self, in_channels = 3, embed_dim = 512, patch_size = 8):
+    def __init__(
+        self,
+        in_channels = 3,
+        embed_dim = 512,
+        patch_size = 8
+    ):
         super(ContentPatching, self).__init__()
-        self.patch_embed = nn.Conv2d(in_channels, out_channels = embed_dim, kernel_size=patch_size, stride=patch_size)
-
-    def forward(self, src_img):
-        # Bicubic interpolation used to downsample from 150x150 into 128x128 for effective maintenance of image properties
-        resized_img = F.interpolate(
-            src_img, 
-            size=(128, 128), 
-            mode='bicubic', 
-            align_corners=False
+        self.patch_embed = nn.Conv2d(
+            in_channels,
+            out_channels=embed_dim,
+            kernel_size=patch_size,
+            stride=patch_size
         )
 
-        patches = self.patch_embed(resized_img)
+    def forward(self, src_img):
+        patches = self.patch_embed(src_img)
         patches = patches.flatten(2)
         patch_embeddings = patches.transpose(1,2)
-
         return patch_embeddings
         
 class StylePatching(nn.Module):
@@ -30,15 +31,24 @@ class StylePatching(nn.Module):
     Style Patching class for patching style image into patches (i,j)
     Style images will be fed in as batch x 3 x 512 x 512
     '''
-    def __init__(self, in_channels = 3, embed_dim = 512, patch_size = 8):
+    def __init__(
+        self,
+        in_channels = 3,
+        embed_dim = 512,
+        patch_size = 8
+    ):
         super(StylePatching, self).__init__()
-        self.patch_embed = nn.Conv2d(in_channels, out_channels = embed_dim, kernel_size=patch_size, stride=patch_size)
+        self.patch_embed = nn.Conv2d(
+            in_channels,
+            out_channels=embed_dim,
+            kernel_size=patch_size,
+            stride=patch_size
+        )
 
     def forward(self, src_img):
         patches = self.patch_embed(src_img)
         patches = patches.flatten(2)
         patch_embeddings = patches.transpose(1,2)
-
         return patch_embeddings
 
         
